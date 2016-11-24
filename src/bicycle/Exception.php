e6423cdb03cae1c368612b1270edaf2fc6c9bd5c
avolkov <alx.volkov@gmail.com>
2016-11-24 23:15:22 +0300

    Add licence

--- src/bicycle/Exception.php
+++ src/bicycle/Exception.php
@@ -1,6 +1,14 @@
 <?php
+/*
+ * (c) Alexander Volkov <alx.volkov@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace bicycle;
 
+/**
+* The root exception.
+*/
 class Exception
 extends \Exception
 {
